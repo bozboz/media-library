3ad11839810c310a299fbd7b5b52980c03853042
Dan Bower <dan.bower88@gmail.com>
2014-04-16 11:09:16 +0100

    Update default InvalidConfigurationException message

--- src/Bozboz/MediaLibrary/Exceptions/InvalidConfigurationException.php
+++ src/Bozboz/MediaLibrary/Exceptions/InvalidConfigurationException.php
@@ -7,7 +7,7 @@ class InvalidConfigurationException extends Exception
 	public function __construct($message = "", $code = 0, Exception $previous = null)
 	{
 		if (empty($message)) {
-			$message = 'Invalid "media" configuration';
+			$message = 'Invalid "media-library" configuration';
 		}
 
 		parent::__construct($message, $code, $previous);

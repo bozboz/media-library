1a11013f0d5a4279ed8a955191c1dd231306cd0b
Mike Street <mike.street@bozboz.co.uk>
2014-04-24 11:51:25 +0100

    Custom masonry layout for media items
    Included masonry.js and custom css for the media overview page

--- src/views/overview.blade.php
+++ src/views/overview.blade.php
@@ -1,27 +1,36 @@
 @extends('admin::overview')
 
+@section('styles')
+@parent
+	<link rel="stylesheet" href="/packages/bozboz/media-library/css/media-library.css">
+@stop
+
 @section('main')
 	@include('admin::partials.new')
 	<h1>{{ $modelName }}</h1>
-	<div class="table-responsive">
-		<table class="table table-striped">
-			<thead>
-			</thead>
-
+	<ul class="js-masonry secret-list" data-masonry-options='{ "columnWidth": 187, "itemSelector": ".masonry-item" }'>
 	@foreach ($instances as $i => $model)
-		<tr>
-		@foreach ($columns[$i] as $name => $value)
-			<td>{{ $value }}</td>
-		@endforeach
-			<td class="no-wrap">
-				{{ Form::model($model, array('action' => array($controller . '@destroy', $model->id), 'method' => 'DELETE')) }}
-					<a class="btn btn-primary btn-sm" href="{{ URL::action($controller . '@edit', array($model->id)) }}"><i class="fa fa-pencil"></i> Edit</a>
+		<li class="masonry-item">
+			<a href="{{ URL::action($controller . '@edit', array($model->id)) }}">
+				{{ $columns[$i]['image'] }}
+			</a>
+			<div class="icons">
+				<a href="{{ URL::action($controller . '@edit', array($model->id)) }}" class="btn btn-info btn-sm pull-right">
+					<i class="fa fa-pencil"></i>
+					Edit
+				</a>
+
+				{{ Form::model($model, array('class' => 'inline-form', 'action' => array($controller . '@destroy', $model->id), 'method' => 'DELETE')) }}
 					<button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-minus-square"></i> Delete</button>
 				{{ Form::close() }}
-			</td>
-		</tr>
-	@endforeach 
-		</table>
-	</div>
+			</div>
+		</li>
+	@endforeach
+	</ul>
 	@include('admin::partials.new')
+@stop
+
+@section('scripts')
+@parent
+	<script src="/packages/bozboz/media-library/js/masonry.js"></script>
 @stop

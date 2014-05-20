077d1b731203087e34a8f44d7aa8c40b8bb1d861
Sam King <sdking02@gmail.com>
2014-05-20 12:32:03 +0100

    Change wording on media widget

--- src/views/fields/media-browser.blade.php
+++ src/views/fields/media-browser.blade.php
@@ -10,21 +10,21 @@ var data = {{ $data }};
 @{{/each}}
 </script>
 
-<button class="btn btn-info js-select-media">Select Media</button>
+<button class="btn btn-info js-select-media">Browse Media</button>
 
 <div class="modal fade media-modal" id="media-modal">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title">Select Media</h4>
+        <h4 class="modal-title">Browse Media</h4>
       </div>
       <div class="modal-body">
 	    <ul class="secret-list"></ul>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" id="update-media">Save changes</button>
+        <button type="button" class="btn btn-primary" id="update-media">Select Media</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
